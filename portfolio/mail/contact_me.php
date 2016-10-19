c742122ebd920379b8007169930175f6c381e502
olagodavid <olagodavid@gmail.com>
2016-10-19 10:49:00 +0300

    php file for contact form functionality

--- portfolio/mail/contact_me.php
+++ portfolio/mail/contact_me.php
@@ -14,4 +14,11 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+
+// Create the email and send the message
+$to = 'olagodavid@gmail.com.com'; // This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@gmail.com\n";
+
  ?>
