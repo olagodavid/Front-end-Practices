9b0479db8dfff61afba18328124a1374bb7e18a9
olagodavid <olagodavid@gmail.com>
2016-10-19 10:46:46 +0300

    php file for contact form functionality

---
+++ portfolio/mail/contact_me.php
@@ -0,0 +1,17 @@
+<?php 
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+ ?>
